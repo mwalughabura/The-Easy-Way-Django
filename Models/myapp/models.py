b4a4e280563afcbbc248191df1b9d19db33e3cc6
Mwalugha <mwalughabura@gmail.com>
2022-08-24 10:43:32 +0300

    I am stuck will back

--- Models/myapp/models.py
+++ Models/myapp/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Flower(models.Model):
-    title = models.charField(max_length=255, default='')
+    title = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.title
--- Models/myapp/views.py
+++ Models/myapp/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'myapp/index.html')
+
+from django.shortcuts import render
+from myapp.models import Flower
+
+def index(request):
+    flowers = Flower.objects.all()
+    
+    return render(request, 'myapp/index.html', {'flowers': flowers })
